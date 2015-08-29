afd00478aa09065ed9e2d82e23eb2b2d5af9d334
markgerow <mark.gerow@openwager.com>
2015-08-29 14:29:39 -0700

    SpikeTrap and TimedTrap

---
+++ CrimsonKeep/Assets/DamageCollisionScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageCollisionScript : MonoBehaviour {
+
+	public float damageToDeal = 10;
+
+	void OnTriggerEnter(Collider other){
+		HealthScript.SetHealth (-10);
+	}
+}
---
+++ CrimsonKeep/Assets/TrapTriggerScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapTriggerScript : MonoBehaviour {
+	Transform child;
+	// Use this for initialization
+	float pressurePlateDelay = 0.5f;
+	float upTime = 0.5f;
+	float downTime = 0.5f;
+	public bool isTimed = true;
+	void Start () {
+		child = transform.GetChild (0);
+		if (isTimed)
+			StartCoroutine ("SpikeLoop");
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if(!isTimed)
+		StartCoroutine ("SpikeMove");
+	}
+
+	IEnumerator SpikeLoop(){
+		while (true) {
+			while (child.localPosition.y < 0) {
+				child.position += new Vector3 (0, 0.05f);
+				yield return new WaitForFixedUpdate ();
+			}
+			yield return new WaitForSeconds (upTime);
+			while (child.localPosition.y > -0.05) {
+				child.position -= new Vector3 (0, 0.05f);
+				yield return new WaitForFixedUpdate ();
+			}
+			yield return new WaitForSeconds (downTime);
+		}
+	}
+
+	IEnumerator SpikeMove(){
+		yield return new WaitForSeconds (pressurePlateDelay);
+		while (child.localPosition.y < 0) {
+			child.position += new Vector3(0, 0.05f);
+			yield return new WaitForFixedUpdate();
+		}
+		yield return new WaitForSeconds (upTime);
+		while (child.localPosition.y > -0.05) {
+			child.position -= new Vector3(0, 0.05f);
+			yield return new WaitForFixedUpdate();
+		}
+		yield return null;
+	}
+}
